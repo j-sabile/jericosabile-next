144d01aafc3f466b6425881bd6b1b6499afaf904
j-sabile <jericosabile@gmail.com>
2025-05-16 17:57:56 +0800

    add google meta

---
+++ src/app/pages/_document.js
@@ -0,0 +1,16 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="google-site-verification" content="lYP3ve-YsFbmxA4OSnghFJWDbX7eI330Kxh8ZAOZBu4" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
